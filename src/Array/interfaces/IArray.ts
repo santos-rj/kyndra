03d6a4362f22332971d6660f96e008a2ad78dbb4
Rafael Santos <santosrj.contato@gmail.com>
2023-08-06 17:11:37 -0300

    docs(array): add description in Array interface and describe insertInLastPosition method

--- src/Array/interfaces/IArray.ts
+++ src/Array/interfaces/IArray.ts
@@ -1,4 +1,16 @@
+/**
+ * Interface IArray<T>
+ *
+ * This interface represents a contract for a data structure that can store elements of type T (generic).
+ * It defines a method to insert an element at the last position of the structure.
+ */
 interface IArray<T> {
+  /**
+   * Inserts an element at the last position of the structure.
+   *
+   * @param _element The element to be inserted.
+   * @returns The element inserted at the last position.
+   */
   insertInLastPosition(_element: T): T;
 }
 

370507f7a674b0d34844a00cd8181b0c4e693bd0
Rafael Santos <santosrj.contato@gmail.com>
2023-08-06 21:12:56 -0300

    chore(array): add insertInFirstPosition method in IArray interface

--- src/Array/interfaces/IArray.ts
+++ src/Array/interfaces/IArray.ts
@@ -2,7 +2,6 @@
  * Interface IArray<T>
  *
  * This interface represents a contract for a data structure that can store elements of type T (generic).
- * It defines a method to insert an element at the last position of the structure.
  */
 interface IArray<T> {
   /**
@@ -12,6 +11,14 @@ interface IArray<T> {
    * @returns The element inserted at the last position.
    */
   insertInLastPosition(_element: T): T;
+
+  /**
+   * Inserts an element at the first position of the structure.
+   *
+   * @param _element The element to be inserted.
+   * @returns The element inserted at the first position.
+   */
+  insertInFirstPosition(_element: T): T;
 }
 
 export default IArray;

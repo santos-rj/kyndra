8866ff68192e0febcce42043a7acea3088bf5e95
Rafael Santos <santosrj.contato@gmail.com>
2023-08-31 19:50:50 -0300

    feat(stack): implements push method in stack class

--- src/Stack/index.ts
+++ src/Stack/index.ts
@@ -1,8 +1,15 @@
 import DataStructure from "../DataStructure";
 
-class Stack<T = number> extends DataStructure<T> {
+import IStack from "./interfaces/IStack";
+("./interfaces/IStack");
+
+class Stack<T = number> extends DataStructure<T> implements IStack<T> {
   constructor(...inputs: T[]) {
     super(inputs);
+  }
+
+  push(...elements: T[]): void {
+    this._data.push(...elements);
   }
 }
 

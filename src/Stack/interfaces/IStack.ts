323e6336bde149b8dcdfef621c7cdd3a3811aae0
Rafael Santos <santosrj.contato@gmail.com>
2023-08-31 19:49:48 -0300

    feat(stack): add push method in IStack interface

--- src/Stack/interfaces/IStack.ts
+++ src/Stack/interfaces/IStack.ts
@@ -1,3 +1,9 @@
-interface IStack<T> {}
+interface IStack<T> {
+  /**
+   * Inserts an element at the top of the stack.
+   * @param elements The element(s) to be inserted.
+   */
+  push(...elements: T[]): void;
+}
 
 export default IStack;

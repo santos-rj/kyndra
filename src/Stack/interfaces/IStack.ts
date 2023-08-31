4cc594ca5df00c8e5603c38550c66b630e1b5e12
Rafael Santos <santosrj.contato@gmail.com>
2023-08-31 20:23:21 -0300

    feat(stack): add pop method in IStack interface

--- src/Stack/interfaces/IStack.ts
+++ src/Stack/interfaces/IStack.ts
@@ -4,6 +4,12 @@ interface IStack<T> {
    * @param elements The element(s) to be inserted.
    */
   push(...elements: T[]): void;
+
+  /**
+   * Removes the element at the top of the stack and returns it.
+   * @returns The element at the top of the stack.
+   */
+  pop(): T | undefined;
 }
 
 export default IStack;
